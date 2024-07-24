743d37f0586bb7d6bc5e45748c4622c2c0d3c09f
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-07-23 21:22:56 -0400

    more worked out example for fun

--- DataStructuresInJava/src/VisitorPattern/Main.java
+++ DataStructuresInJava/src/VisitorPattern/Main.java
@@ -2,6 +2,7 @@ package VisitorPattern;
 
 public class Main {
     public static void main(String[] args) {
+        //Make Your Elements
         FoodItemElement apple = new ConcreteFruit("Apple", 95);
         FoodItemElement carrot = new ConcreteVegetable("Carrot", 25);
         FoodItemElement chicken = new ConcreteMeat("Chicken", 335);
@@ -10,9 +11,19 @@ public class Main {
         System.out.println(carrot);
         System.out.println(chicken);
 
+        //Make Your Visitors You're Going To Use
         FoodVisitor nutritionalValueVisitor = new NutritionalValueVisitor();
         FoodVisitor printDetailsVisitor = new PrintDetailsVisitor();
 
+        System.out.println("\nAn Example With An APPLE!!!!!!\n");
+        apple.accept(nutritionalValueVisitor);
+        apple.accept(printDetailsVisitor);
+
+        System.out.println("\nAn Example With A CHICKEN!!!!!!\n");
+        chicken.accept(nutritionalValueVisitor);
+        chicken.accept(printDetailsVisitor);
+
+        System.out.println("\nITERATION!!!!!\n");
         FoodItemElement[] foodItems = { apple, carrot, chicken };
 
         for (FoodItemElement foodItem : foodItems) {
